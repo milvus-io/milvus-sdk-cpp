9591c750b1bafec5247f73aaa0627f5bb40213de
Ji Bin <matrixji@live.com>
2021-12-07 10:17:51 +0800

    Support centos7 as builder (#55)
    - support build/test from centos7
    - using container as builder on github
    
    Related Issue: #51
    
    Signed-off-by: Ji Bin <matrixji@live.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+check_sudo() {
+    if [ -x "$(command -v sudo)" ] ; then
+        export SUDO=sudo
+    else
+        export SUDO=
+    fi
+}
+
 get_cmake_version() {
-    if which cmake 2>/dev/null 1>/dev/null ; then
+    if [ -x "$(command -v cmake)" ] ; then
         expr $(cmake --version | grep version | sed 's/\./ /g' | awk '{printf "%02d%02d", $3, $4}')
     else
         echo 0
@@ -9,29 +17,47 @@ get_cmake_version() {
 }
 
 install_deps_for_ubuntu_1804() {
-    sudo apt-get update
-    sudo apt-get -y install python2.7 gpg wget gcc g++ ccache clang-format-10 clang-tidy-10
-    sudo apt-get -y install libssl-dev iwyu
+    check_sudo
+    ${SUDO} apt-get update
+    ${SUDO} apt-get -y install python2.7 gpg wget gcc g++ ccache make clang-format-10 clang-tidy-10 \
+                       libssl-dev iwyu
 
     # for cmake >= 3.12
     current_cmake_version=$(get_cmake_version)
     if [ $current_cmake_version -lt 312 ] ; then
-        sudo rm -f /usr/share/keyrings/kitware-archive-keyring.gpg /etc/apt/sources.list.d/kitware.list
-        wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
-        echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
-        sudo apt-get update
-        sudo apt-get -y install kitware-archive-keyring
-        sudo apt-get -y install cmake
+        ${SUDO} rm -f /usr/share/keyrings/kitware-archive-keyring.gpg /etc/apt/sources.list.d/kitware.list
+        wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | ${SUDO} tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+        echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic main' | ${SUDO} tee /etc/apt/sources.list.d/kitware.list >/dev/null
+        ${SUDO} apt-get update
+        ${SUDO} apt-get -y install kitware-archive-keyring
+        ${SUDO} apt-get -y install cmake
+    fi
+}
+
+install_deps_for_centos_7() {
+    check_sudo
+    ${SUDO} yum -y update
+    ${SUDO} yum -y install epel-release
+    yum -y install gcc gcc-c++ python gpg wget ccache make openssl-devel which
+    
+    # for cmake >= 3.12, using cmake3 from epel
+    current_cmake_version=$(get_cmake_version)
+    if [ $current_cmake_version -lt 312 ] ; then
+        ${SUDO} yum -y install cmake3
+        test -L /usr/local/bin/cmake && ${SUDO} unlink /usr/local/bin/cmake
+        ${SUDO} ln -s /usr/bin/cmake3 /usr/local/bin/cmake
     fi
 }
 
 
 if [ -x "$(command -v apt)" ]; then
     # for ubuntu
-    if lsb_release -r | grep -q 18.04 ; then
+    if grep -q 'Ubuntu 18.04' /etc/issue ; then
         install_deps_for_ubuntu_1804
     fi
 elif [ -x "$(command -v yum)" ] ; then
-    # todo handle for centos7
-    :
+    # for os support yum
+    if grep -q 'CentOS Linux release 7' /etc/redhat-release ; then
+        install_deps_for_centos_7
+    fi
 fi
