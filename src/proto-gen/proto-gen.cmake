26b260dc74a520c8cb4a821e21912d2dfd7c8d5d
groot <yihua.mo@zilliz.com>
2022-03-17 14:17:18 +0800

    Add milvus-proto as proto source (#167)
    Signed-off-by: groot <yihua.mo@zilliz.com>

--- src/proto-gen/proto-gen.cmake
+++ src/proto-gen/proto-gen.cmake
@@ -17,24 +17,24 @@
 function(add_proto_source name)
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/proto-gen/${name}.pb.cc
-        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/proto/${name}.proto
+        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/milvus-proto/proto/${name}.proto
                 ${protobuf_BINARY_DIR}/protoc${CMAKE_EXECUTABLE_SUFFIX}
         COMMAND ${protobuf_BINARY_DIR}/protoc${CMAKE_EXECUTABLE_SUFFIX}
-                --cpp_out ${CMAKE_CURRENT_SOURCE_DIR}/proto-gen -I${CMAKE_CURRENT_SOURCE_DIR}/proto
-                ${CMAKE_CURRENT_SOURCE_DIR}/proto/${name}.proto
+                --cpp_out ${CMAKE_CURRENT_SOURCE_DIR}/proto-gen -I${CMAKE_CURRENT_SOURCE_DIR}/milvus-proto/proto
+                ${CMAKE_CURRENT_SOURCE_DIR}/milvus-proto/proto/${name}.proto
     )
 endfunction(add_proto_source name)
 
 function(add_proto_service name)
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/proto-gen/${name}.grpc.pb.cc
-        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/proto/${name}.proto
+        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/milvus-proto/proto/${name}.proto
                 ${protobuf_BINARY_DIR}/protoc${CMAKE_EXECUTABLE_SUFFIX}
                 ${grpc_BINARY_DIR}/grpc_cpp_plugin${CMAKE_EXECUTABLE_SUFFIX}
         COMMAND ${protobuf_BINARY_DIR}/protoc${CMAKE_EXECUTABLE_SUFFIX}
-                --grpc_out ${CMAKE_CURRENT_SOURCE_DIR}/proto-gen -I${CMAKE_CURRENT_SOURCE_DIR}/proto
+                --grpc_out ${CMAKE_CURRENT_SOURCE_DIR}/proto-gen -I${CMAKE_CURRENT_SOURCE_DIR}/milvus-proto/proto
                 --plugin=protoc-gen-grpc=${grpc_BINARY_DIR}/grpc_cpp_plugin${CMAKE_EXECUTABLE_SUFFIX}
-                ${CMAKE_CURRENT_SOURCE_DIR}/proto/${name}.proto
+                ${CMAKE_CURRENT_SOURCE_DIR}/milvus-proto/proto/${name}.proto
     )
 endfunction(add_proto_service name)
 
